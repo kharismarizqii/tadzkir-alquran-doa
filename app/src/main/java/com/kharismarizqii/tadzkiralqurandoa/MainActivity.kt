dc3b5580ce641eff8ec85c6883b1627f28b85d40
kharismarizqii <rizqikharisma@gmail.com>
2021-01-28 18:03:48 +0700

    add menu support

--- app/src/main/java/com/kharismarizqii/tadzkiralqurandoa/MainActivity.kt
+++ app/src/main/java/com/kharismarizqii/tadzkiralqurandoa/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.kharismarizqii.tadzkiralqurandoa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kharismarizqii.tadzkiralqurandoa.asmaulhusna.AsmaulActivity
+import com.kharismarizqii.tadzkiralqurandoa.bacaanshalat.BacaanShalatActivity
 import com.kharismarizqii.tadzkiralqurandoa.databinding.ActivityMainBinding
+import com.kharismarizqii.tadzkiralqurandoa.doaharian.DoaHarianActivity
+import com.kharismarizqii.tadzkiralqurandoa.tahlil.TahlilActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +17,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setButtonMenu()
+    }
+
+    private fun setButtonMenu() {
+        binding.apply {
+            btnAsmaul.setOnClickListener {
+                Intent(this@MainActivity, AsmaulActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+            btnBacaanShalat.setOnClickListener {
+                Intent(this@MainActivity, BacaanShalatActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+            btnDoaHarian.setOnClickListener {
+                Intent(this@MainActivity, DoaHarianActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+            btnDoaTahlil.setOnClickListener {
+                Intent(this@MainActivity, TahlilActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }
     }
 }
